da16a7b8d13fc9683fe129abbad62cdf3f8b3e53
Narasimha kambham <viswakambham@gmail.com>
2025-10-07 17:12:56 +0530

    feat: add treasure island game with interactive choices
    Implement a text-based adventure game where players navigate through different scenarios (forest, cave, beach) with various outcomes based on their choices. Includes ASCII art for visual elements and multiple endings.

---
+++ Day_03/README.md
@@ -0,0 +1,22 @@
+# Day 03: Control Flow and Logical Operators
+
+## Overview
+This project, developed as part of "The 100 Days of Python" challenge, focuses on control flow with if/else statements, conditional operators, nested if/elif statements, multiple if statements, and logical operators to create an interactive Treasure Island game.
+
+## Concepts Covered
+- Control Flow with if / else and Conditional Operators
+- Introducing the Modulo
+- Nested if statements and elif statements
+- Multiple If Statements in Succession
+- Logical Operators
+
+## Project: Treasure Island
+This program guides the user through a series of choices on Treasure Island, leading to different outcomes based on their decisions. The goal is to find the treasure.
+
+### How to Use
+1. Run the `main.py` script.
+2. Follow the prompts to make choices that will determine your fate on Treasure Island.
+3. Navigate through the challenges to find the treasure.
+
+## Example Interaction
+
---
+++ Day_03/main.py
@@ -0,0 +1,378 @@
+import time
+
+def pause(text):
+    print(text)
+    time.sleep(1.2)
+
+def game_over():
+    print(r'''
+                             ,--. 
+                            {    } 
+                            K,   } 
+                           /  ~Y` 
+                      ,   /   / 
+                     {_'-K.__/ 
+                       `/-.__L._ 
+                       /  ' /`\_} 
+                      /  ' / 
+              ____   /  ' / 
+       ,-'~~~~    ~~/  ' /_ 
+     ,'             ``~~~  ', 
+    (                        Y 
+   {                         I 
+  {      -                    `, 
+  |       ',                   ) 
+  |        |   ,..__      __. Y 
+  |    .,_./  Y ' / ^Y   J   )| 
+  \           |' /   |   |   || 
+   \          L_/    . _ (_,.'( 
+    \,   ,      ^^""' / |      ) 
+      \_  \          /,L]     / 
+        '-_~-,       ` `   ./` 
+           `'{_            ) 
+               ^^\..___,.--`     
+    ''')
+    print("GAME OVER")
+
+def dragon_appears():
+    print(r'''
+                          j                       k 
+                         .K                       Z. 
+                         jM.                     .Mk 
+                         WMk                     jMW 
+                         YMM.       ,,,,,,      .MMY 
+                         `MML;:''```      ```':;JMM' 
+                         /`JMMMk.           .jMMMk'\ 
+                         / `GMMMI'         `IMMMO' \ 
+                        /    ~~~'           `~~~    \ 
+                        /                           \ 
+                        |                           | 
+                        |      ;,           ,;      | 
+                        |      Tk           jT      | 
+                         |     `Mk   . .   jM'     | 
+                         |      YK.   Y   .ZY      | 
+                          \     `Kk   |   jZ'     / 
+                          \       `'  |  `'       / 
+                           \          |          / 
+                            \         |         / 
+                            \         |         / 
+                             \        |        / 
+                              \       |       / 
+                              \       |       / 
+                               \      |      / 
+                                \     |     / 
+                                \  |  |  |  / 
+                                 \ {| | |} / 
+                                  \ ` | ' / 
+                                   \  |  / 
+                                   \  |  / 
+                                    \   / 
+                                     \ / 
+                                      ~
+    ''')
+def potion():
+    print(r'''
+      _____
+     `.___,'
+      (___)
+      <   >
+       ) (
+      /`-.\
+     /     \
+    / _    _\
+   :,' `-.' `:
+   |         |
+   :         ;
+    \       /
+     `.___.' 
+     
+    ''')
+def sword():
+    print(r'''
+    
+
+                                                      )         
+                                                        (            
+                                                      '    }      
+                                                    (    '      
+                                                   '      (   
+                                                    )  |    ) 
+                                                  '   /|\    `
+                                                 )   / | \  ` )   
+                                                {    | | |  {   
+                                               }     | | |  .
+                                                '    | | |    )
+                                               (    /| | |\    .
+                                                .  / | | | \  (
+                                              }    \ \ | / /  .        
+                                               (    \ `-' /    }
+                                               '    / ,-. \    ' 
+                                                }  / / | \ \  }
+                                               '   \ | | | /   } 
+                                                (   \| | |/  (
+                                                  )  | | |  )
+                                                  .  | | |  '
+                                                     J | L
+                                               /|    J_|_L    |\
+                                               \ \___/ o \___/ /
+                                                \_____ _ _____/
+                                                      |-|
+                                                      |-|
+                                                      |-|
+                                                     ,'-'.
+                                                     '---'
+            
+    ''')
+def win():
+    print(r'''
+    
+                            _.--.
+                        _.-'_:-'||
+                    _.-'_.-::::'||
+               _.-:'_.-::::::'  ||
+             .'`-.-:::::::'     ||
+            /.'`;|:::::::'      ||_
+           ||   ||::::::'     _.;._'-._
+           ||   ||:::::'  _.-!oo @.!-._'-.
+           \`.  ||:::::.-!()oo @!()@.-'_.|
+            '.'-;|:.-'.&$@.& ()$%-'o.'\U||
+              `>'-.!@%()@'@_%-'_.-o _.|'||
+               ||-._'-.@.-'_.-' _.-o  |'||
+               ||=[ '-._.-\U/.-'    o |'||
+               || '-.]=|| |'|      o  |'||
+               ||      || |'|        _| ';
+               ||      || |'|    _.-'_.-'
+               |'-._   || |'|_.-'_.-'
+            jgs '-._'-.|| |' `_.-'
+                    '-.||_/.-'
+    ''')
+def snake():
+    print('''
+       ---_ ......._-_--.
+      (|\ /      / /| \  \
+      /  /     .'  -=-'   `.
+     /  /    .'             )
+   _/  /   .'        _.)   /
+  / o   o        _.-' /  .'
+  \          _.-'    / .'*|
+   \______.-'//    .'.' \*|
+    \|  \ | //   .'.' _ |*|
+     `   \|//  .'.'_ _ _|*|
+      .  .// .'.' | _ _ \*|
+      \`-|\_/ /    \ _ _ \*\
+       `/'\__/      \ _ _ \*\
+      /^|            \ _ _ \*
+     '  `             \ _ _ \      
+                       \_
+    ''')
+def fireflies():
+    print('''
+          ཐི༏ཋྀ         ཐི༏ཋྀ      ཐི༏ཋྀ
+  ཐི༏ཋྀ    ཐི༏ཋྀ           ཐི༏ཋྀ
+        ཐི༏ཋྀ   ཐི༏ཋྀ        ཐི༏ཋྀ
+ ཐི༏ཋྀ             ཐི༏ཋྀ       ཐི༏ཋྀ
+
+    ''')
+def torch():
+    print(r'''
+             /|
+        /\/ |/\
+        \  ^   | /\  /\
+  (\/\  / ^   /\/  )/^ )
+   \  \/^ /\       ^  /
+    )^       ^ \     (
+   (   ^   ^      ^\  )
+    \___\/____/______/
+    [________________]
+     |              |
+     |     //\\     |
+     |    <<()>>    |
+     |     \\//     |
+      \____________/
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+    ''')
+def bat():
+    print(r'''
+    
+    ._.                  _.____.
+     ) \.              /    .(
+     )  |            .'   .(
+     ). ).          .'  .(
+       ) |.        .'  (
+       ). ;      ./  .(
+        ) |      )  (
+        ).;      :.(
+         )|    .|.;
+         .^--^./ (.
+         ;0..0;   \
+          'vv'_.:_.;     
+               m  M
+    ''')
+def shell():
+    print(r'''
+           _.-''|''-._
+        .-'     |     `-.
+      .'\       |       /`.
+    .'   \      |      /   `.
+    \     \     |     /     /
+     `\    \    |    /    /'
+       `\   \   |   /   /'
+         `\  \  |  /  /'
+        _.-`\ \ | / /'-._
+       {_____`\\|//'_____}
+               `-'
+
+    ''')
+def boat():
+    print('''
+    
+
+                                                ::
+                               .-'`""::..       ::
+                             ,'        `""::..  ::
+                           ,'               `""::..
+                          ,                      `""::..
+                         :                            `""::..
+                        :                                 `""::.
+                       :                                      .'
+                      :                                     .'
+                      :                                   .'
+     :.              :                                   '
+     ::              :                                 .'
+     |:             :                                 :
+     ||             :       ____                     :
+     |:.            :   ..""    ""-.                :
+     ||:.           : .'            `.             :
+     || `:.          :                `.          :
+   __||  `""::..     :                 :.         :
+.-"  :|       `""::..`:                ::.       :
+     ::            `""::..            ::  .      :
+     `:.                `""::..       ::   :     :           __
+      `:.                    `""::.. ::_____:____:__    _.--'
+   ___.-`::..                  _.-"""""             """"----....
+      `--._`"::.           _.-'
+               `"":: ___.-'
+            ____--"""
+    ''')
+while True:
+    print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_______/
+*******************************************************************************
+    ''')
+    pause(r"""
+    ~ ~ ~ WELCOME TO SKULL ISLAND ~ ~ ~
+         ~ ~ ~ ~ ~ ~ ~ ~ ~
+        ~   SKULL ISLAND   ~
+         ~ ~ ~ ~ ~ ~ ~ ~ ~
+            /     |     \
+    🌲Forest   🪨 Cave   🦴 Beach
+    """)
+    choice1 = input("Where do you want to go? (forest/cave/beach/quit): ").lower()
+    if choice1 == "quit":
+        print("You quit the game.")
+        break   
+    elif choice1 == "forest":
+        pause("You entered the Forest of Whispers... 🌲")
+        pause("You hear strange noises around you.")
+        forest_choice = input("1. Follow the fireflies ✨\n2. Hide behind a tree 🌳\nEnter 1 or 2: ")
+        if forest_choice == "1":
+            fireflies()
+            pause("The fireflies lead you to a hidden cave 🪨...")
+            pause("Inside the cave, you find two mysterious objects:")
+            item = input("Choose one: 1. Unknown Potion 🧪 or 2. Unknown Stick 🪄\nEnter 1 or 2: ")
+
+            dragon_appears()
+            pause("The dragon is here!")
+            
+            if item == "1":
+                pause("You quickly drink the potion...")
+                potion()
+                pause("Oh no! It was just water 💧")
+                pause("")
+                pause("The dragon roars and burns you to ashes ☠️")
+                game_over()
+                break
+            elif item == "2":
+                pause("You raise the stick high...")
+                pause("It transforms into a glowing sword ⚔️✨")
+                sword()
+                pause("The sword gleams!")
+                pause("You slay the dragon heroically!")
+                pause("Behind it lies a golden treasure chest 💰")
+                win()
+                pause("Victory!")
+                pause("🏆 YOU FOUND THE TREASURE! Congratulations! 🎉")
+                break
+            else:
+                pause("You hesitated... the dragon attacked! ☠️")
+                game_over()
+                break
+        else:
+            pause("You hide behind a tree, but a poisonous snake bites you! 🐍")
+            snake()
+            pause("The snake strikes!")
+            pause("The venom spreads quickly... ☠️")
+            game_over()
+            pause("Game Over!")
+            break
+    elif choice1 == "cave":
+        pause("You entered the dark Cave of Echoes 🪨...")
+        cave_choice = input("1. Light a torch 🔥\n2. Walk in darkness 👣\nEnter 1 or 2: ")
+
+        if cave_choice == "1":
+            pause("You light the torch... Bats swarm around! 🦇")
+            torch()
+            pause("The torch illuminates the cave.")
+            bat()
+            pause("Bats scatter!")
+            pause("You run back to the island start! 🔁")
+            continue
+        else:
+            pause("You step forward and fall into a pit! ☠️")
+            pause("You die from the fall.")
+            game_over()
+            break
+    elif choice1 == "beach":
+        pause("You walk along the Beach of Bones 🦴...")
+        beach_choice = input("1. Dig near a glowing shell 🐚\n2. Sail on a broken boat 🛶\nEnter 1 or 2: ")
+
+        if beach_choice == "1":
+            shell()
+            pause("A beautiful shell!")
+            pause("You dig and find half of a treasure map! 🗺️")
+            pause("But the tide rises, forcing you back! 🔁")
+            continue
+        else:
+            pause("You sail away on the broken boat...")
+            boat()
+            pause("The boat sets sail...")
+            pause("It sinks halfway! ☠️")
+            game_over()
+            pause("Game Over!")
+            break
+    else:
+        pause("Invalid choice, try again.")
